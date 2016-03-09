054f34081a43cafab847df150b034f4e0444248e
Ivan Krpan <ivan.krpan29@gmail.com>
2016-03-09 13:36:25 +0100

    Dodali smo još jednu varijablu

--- index.php
+++ index.php
@@ -2,12 +2,27 @@
 <html>
 <head>
     <title>Prvi PHP</title>
+    <meta charset="utf-8"
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
-<P>
+<?php
+    echo('Ovo je prije odlomka<br>');
+    echo('<br>');
+    echo('Super, jos cemo dodati');
+    echo('<br>');
+    echo(date ('d.m.Y'));
+    echo('<br>');
+    $ocjena=5;
+    echo('<br>Vaša ocjena je:' . $ocjena);
+    echo('<br>');
+    $sifra=23524;
+    echo('<br>Vaša šifra je:' . $sifra);
+?>
+<p>
     Ovo je odlomak
-</P>
+</p>
+
 </body>
 </html>
